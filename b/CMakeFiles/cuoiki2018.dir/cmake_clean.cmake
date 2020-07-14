c6213d7f76f4179efed5a027e4d2def5c46da2b1
phungson <you@example.com>
2020-07-14 20:23:51 +0700

    them bai 20182

---
+++ b/CMakeFiles/cuoiki2018.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cuoiki2018.dir/ex/cuoiki2018.c.o"
+  "cuoiki2018.pdb"
+  "cuoiki2018"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cuoiki2018.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- ex/cuoiki2018.c
+++ ex/cuoiki2018.c
@@ -5,10 +5,27 @@
 #include"libfdr/jval.h"
 #include"igraph/igraph.h"
 #include"DebugPrintf/debug_printf.h"
+#include"c-vector/cvector.h"
 #define MAX 9
 int so_thanh_tri;
 int so_duong;
 int ma_tran_ke[MAX][MAX];
+long int print_range(igraph_vector_t *e, igraph_vector_t *w) {
+    long int i,a; 
+    long int sum = 0;
+    for (i = 0; i < igraph_vector_size(e); i++) {
+        a=(long int) VECTOR(*e)[i];
+        sum += (long int)VECTOR(*w)[a];    
+    }
+    return sum;
+}
+void print_vector(igraph_vector_t *v) {
+    long int i; 
+    for (i = 0; i < igraph_vector_size(v); i++) {
+        printf("%ld ",(long int) VECTOR(*v)[i]);
+    }
+    printf("\n");
+}
 int case1(){
     int m,n,z,k=0;
     memset(ma_tran_ke,0,4*MAX*MAX);
@@ -25,21 +42,22 @@ int case1(){
         fscanf(p,"%d",&n);
         fscanf(p,"%d",&z);
         ma_tran_ke[m][n] = z;
+        ma_tran_ke[n][m] = z;
     }
-    for(int j =0 ; j< MAX ;j++)
+    for(int j =0 ; j< so_thanh_tri+1 ;j++)
     {
         ma_tran_ke[0][j]=k;
         k++;
     }
     k=0;
-    for(int i =0 ; i< MAX ;i++)
+    for(int i =0 ; i< so_thanh_tri+1 ;i++)
     {
         ma_tran_ke[i][0]=k;
         k++;
     }
-    for(int i =0; i < MAX ;i++)
+    for(int i =0; i < so_thanh_tri+1 ;i++)
     {
-        for(int j=0;j<MAX;j++)
+        for(int j=0;j<so_thanh_tri + 1;j++)
         {
             printf("%5d",ma_tran_ke[i][j]);
         }
@@ -47,14 +65,110 @@ int case1(){
     }
 }
 int case2(){
-    for(int i=1;i< MAX -1 ;i++)
+    int danhsach2[MAX];
+    memset(danhsach2,0,4*MAX);
+    for(int i=1;i< so_thanh_tri + 1 ;i++)
     {
-        for (int j = 0; j < MAX -1 ; j++)
+        for (int j = 1; j < so_thanh_tri + 1 ; j++)
         {
-            
+            if(ma_tran_ke[i][j] != 0)
+                danhsach2[j] = j;
         }
-        
+        printf("Castle %d: ",i);
+        for(int j=1 ; j < so_thanh_tri + 1 ; j ++ )
+            {
+                if(danhsach2[j] != 0)
+                    printf("%d ",danhsach2[j]);
+            }
+        printf("\n");
+        memset(danhsach2,0,4*MAX);
     }
+}
+int case31(){
+    int check = 1;
+    cvector_vector_type(int) v = NULL;
+    for(int i=1;i< so_thanh_tri + 1 ;i++)
+    {
+        for (int j = 1; j < so_thanh_tri + 1 ; j++)
+        {
+            if(ma_tran_ke[i][j] < 50 && ma_tran_ke[i][j] !=0)
+            {
+                check = 0;
+                break;
+            }
+        }
+        if(check == 1)
+        {
+            cvector_push_back(v,i);
+        }
+        check = 1;
+    }
+    for (int i = 0; i < cvector_size(v); ++i) {
+    printf("%d ",v[i]);
+  }
+    printf("\n");
+}
+int case4(){
+    int xuatphat,dich,sothanhtrimoi,soduongdimoi,a;
+    printf("Nhap vao thanh tri xuat phat: ");
+    scanf("%d",&xuatphat);
+    printf("Nhap vao thanh tri dich: ");
+    scanf("%d",&dich);
+    FILE* p = fopen("/home/phungson/hyouteisan/ex/dothi1.txt","r");
+    fscanf(p,"%d",&sothanhtrimoi);
+    fscanf(p,"%d",&soduongdimoi);
+    igraph_t g;
+    igraph_vector_t thanhtri;
+    igraph_vector_t v;
+    igraph_vector_t e;
+    igraph_vector_t w;
+    igraph_vector_init(&thanhtri,0);
+    igraph_vector_init(&v,so_thanh_tri);
+    igraph_vector_init(&e,so_thanh_tri/2);
+    igraph_vector_init(&w,0);
+    while(fscanf(p,"%d",&a)!= EOF)
+    {
+        igraph_vector_push_back(&thanhtri, a);
+        fscanf(p,"%d",&a);
+        igraph_vector_push_back(&thanhtri, a);
+        fscanf(p,"%d",&a);
+        igraph_vector_push_back(&w,a);
+    }
+    igraph_create(&g,&thanhtri,0,1);
+    igraph_get_shortest_path_dijkstra(&g,&v,&e,xuatphat,dich,&w,IGRAPH_ALL);
+    printf("%ld\n",print_range(&e,&w));
+    print_vector(&v);
+}
+int case5(){
+    int xuatphat,dich;
+    printf("Nhap vao thanh tri xuat phat: ");
+    scanf("%d",&xuatphat);
+    printf("Nhap vao thanh tri dich: ");
+    scanf("%d",&dich);
+    igraph_t g;
+    igraph_vector_t thanhtri;
+    igraph_vector_t v;
+    igraph_vector_t e;
+    igraph_vector_t w;
+    igraph_vector_init(&thanhtri,0);
+    igraph_vector_init(&v,so_thanh_tri);
+    igraph_vector_init(&e,so_thanh_tri/2);
+    igraph_vector_init(&w,0);
+    for(int i =1; i < so_thanh_tri+1 ;i++)
+    {
+        for(int j=1;j<so_thanh_tri + 1;j++)
+        {
+            if(ma_tran_ke[i][j] >= 50){
+                igraph_vector_push_back(&thanhtri, i);
+                igraph_vector_push_back(&thanhtri, j);
+                igraph_vector_push_back(&w,ma_tran_ke[i][j]);
+            }
+        }
+    }
+    igraph_create(&g,&thanhtri,0,1);
+    igraph_get_shortest_path_dijkstra(&g,&v,&e,xuatphat,dich,&w,IGRAPH_ALL);
+    printf("%ld\n",print_range(&e,&w));
+    print_vector(&v);
 }
 int main(){
     int chon;
@@ -72,6 +186,10 @@ int main(){
     switch (chon)
     {
     case 1: case1()    ;   break; 
+    case 2 : case2(); break;
+    case 31 : case31() ; break;
+    case 4 : case4() ; break;
+    case 5 : case5() ; break;
     case 6: break; 
     default: printf("So ban chon khong co trong menu. Moi nhap lai\n");break;
     }
